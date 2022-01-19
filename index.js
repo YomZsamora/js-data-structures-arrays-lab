0a52aeb8240f897344de41781b02d51734e80cd6
yomZsamora <samora.y@adzumi.co.ke>
2022-01-19 07:18:22 +0300

    Function that appends pet Odie to the end of the pets array.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution here!
+// List of Pets
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"]; 
+const removeLast = ["Milo", "Otis", "Garfield"]; 
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+// Appends pet "Odie" to the end of the pets array
+let appendPet = () => {
+   return append.push("Odie");
+}
