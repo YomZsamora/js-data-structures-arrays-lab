1d699a220c8272f55f654620ef479900a5d93c2d
yomZsamora <samora.y@adzumi.co.ke>
2022-01-19 07:24:56 +0300

    Refactored code functions.

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
 // Appends pet "Odie" to the end of the pets array
-let appendPet = () => {
-   return append.push("Odie");
-}
+let appendPet = () => append.push("Odie")
+
 
 // Prepends a pet to the beginning of the pets array
-let prependPet = () => {
-   return prepend.unshift("Odie");
-}
+let prependPet = () => prepend.unshift("Odie")
